31d89ce875a6a5e08ef6b3e9a569643a1711fd5c
Luis Martinez <s_k_ap3@hotmail.com>
2021-05-20 21:50:46 -0500

    clase 9 creando layout de productos...

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const products = [
       {
             name: "Red Shoes",
-            proce: 75
+            proce: 75,
+            image: "https://image.shutterstock.com/image-photo/studio-shot-pair-running-shoes-260nw-1415726975.jpg"
       },
       {
             name: "black bike",
-            price: 300
+            price: 300,
+            image: "https://www.santafixie.com/en/media/catalog/product/f/u/fuji-declaration-satin-black-1b.jpg"
       }
 ]
 router.get('/',function(req,res){
